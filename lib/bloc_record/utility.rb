0ff0eb56b313f1b40d798fbadf3d9feafad35e62
Alex Rolfe <rolfepercussion@gmail.com>
2016-09-14 22:51:38 -0500

    Adds base.rb as a base class for users to interact with the database

--- lib/bloc_record/utility.rb
+++ lib/bloc_record/utility.rb
@@ -8,6 +8,22 @@ module BlocRecord
       string.gsub!(/([a-z/d])([A-Z])/,'/1_/2')
       string.tr!("-", "_")
       string.downcase
-    end    
+    end
+
+    def sql_strings(value)
+      case value
+      when String
+        "'#{value}'"
+      when Numeric
+        value.to_s
+      else
+        "null"
+      end
+    end
+
+    def convert_keys(options)
+      options.keys.each {|k| option[k.to_s] = options.delete(k) if k.kind_of?(Symbol)}
+      options
+    end
   end
 end
