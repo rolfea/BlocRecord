361c07db118ebf5fcf99cc02851b42494dfacf3a
Alex Rolfe <rolfepercussion@gmail.com>
2016-09-19 17:56:06 -0500

    Adds find method to selection which returns multiple rows if present

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -1,7 +1,20 @@
 require 'sqlite3'
 
 module Selection
-  def find(id)
+  def find(#ids)
+    if ids.length == 1
+      find_one(ids.first)
+    else
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        WHERE id IN (#{ids.join(",")});
+      SQL
+
+      rows_to_array(rows)
+    end
+  end
+  
+  def find_one(id)
     row = connection.get_first_row <<-SQL
       SELECT #{columns.join(",")} FROM #{table}
       WHERE id = #{id};
