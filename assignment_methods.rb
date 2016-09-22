b8121896a510903fe363974e806684b2c931b63b
Alex Rolfe <rolfepercussion@gmail.com>
2016-09-22 10:29:29 -0500

    Corrects find_by method to use less specific method paramters (attribute/value) in assignment_methods.rb

--- assignment_methods.rb
+++ assignment_methods.rb
@@ -2,10 +2,10 @@ def snake_to_camel(snake_string)
   string = snake_string.gsub(/(_[a-z])/) {|match| match[1].upcase}
 end
 
-def find_by(contact, contact_id)
+def find_by(attribute, value)
   results = connection.execute <<-SQL
-    SELECT #{contact} FROM #{table}
-    WHERE contact_id = #{contact_id}
+    SELECT #{attribute} FROM #{table}
+    WHERE value = #{value}
   SQL
   results
 end
