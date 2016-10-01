22aeb0e7f0e2dada31da3486063de3a89378d3d7
Alex Rolfe <rolfepercussion@gmail.com>
2016-10-01 06:59:34 -0500

    Adds support for nested joins in selection.rb

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -84,10 +84,108 @@ module Selection
     rows_to_array(rows)
   end
 
-  private
-  def method_missing(find_by, *args, &block)
+  def where(*args)
+    # check for nil
+    if args.length == 0 # where returns object with a #not method
+    def not(*args) # does where execute the conditional first if no params passed to it?
+      if args.count > 1
+        expression = args.shift
+        expression.insert(0, "!") # flip the expression for negation
+        params = args
+      else
+        case args.first
+        when String
+          expression = args.first
+          expression.insert(0, "!") # flip the expression for negation
+        when Hash
+          expression_hash = BlocRecord::Utility.convert_keys(args.first)
+          expression = expression_hash.map { |key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" AND ")
+          expression.insert(0, "!") # flip the expression for negation
+        end
+      end
 
+      sql = <<-SQL
+        SELECT #{columns.join(",")} FROM #{table}
+        WHERE #{expression};
+      SQL
+
+      rows = connection.execute(sql, params)
+      return rows_to_array(rows)
+    end
+
+    if args.count > 1
+      expression = args.shift
+      params = args
+    else
+      case args.first
+      when String
+        expression = args.first
+      when Hash
+        expression_hash = BlocRecord::Utility.convert_keys(args.first)
+        expression = expression_hash.map { |key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+      end
+    end
+
+    sql = <<-SQL
+      SELECT #{columns.join(",")} FROM #{table}
+      WHERE #{expression};
+    SQL
+
+    rows = connection.execute(sql, params)
+    rows_to_array(rows)
   end
+
+def order(*args)
+    if args.count > 1
+      order = args.join(",")
+      order.each_with_index do |param, index|
+        # multiple ordering conditions wrapped in (), separated by ,
+        if order[index] == "ASC" || order[index] == "DESC"
+          order[index] << "),"
+          order[index - 1].insert(0, "(")
+        end
+      end
+    else
+      order = args.first.to_s
+    end
+    rows = connection.execute <<-SQL
+      SELECT * FROM #{table}
+      ORDER BY #{order};
+    SQL
+    rows_to_arrays(rows)
+  end
+
+  def join(*args)
+    if args.count > 1
+      joins = args.map { |arg| "INNER JOIN #{arg} ON #{arg}.#{table}_id = #{table}.id"}.join(" ")
+      rows = connection.execute <<-SQL
+        SELECT * FROM #{table} #{joins}
+      SQL
+    else
+      case arg.first
+      when String
+        rows = connection.execute <<-SQL
+          SELECT * FROM #{table} #{BlocRecord::Utility.sql_strings(arg)};
+        SQL
+      when Symbol
+        rows = connection.execute <<-SQL
+          SELECT * FROM #{table}
+          INNER JOIN #{arg} ON #{arg}.#{table}_id = #{table}.id
+        SQL
+      when Hash
+        expression_hash = BlocRecord::Utility.convert_keys(args.first)
+        expression = expression_hash.map { |key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(",")
+        rows = connection.execute <<-SQL
+          SELECT * FROM #{table}
+          INNER JOIN #{expression[0]} ON #{expression[0]}.#{table}_id = #{table}.id
+          INNER JOIN #{expression[1]} ON #{expression[1]}.#{table}_id = #{table}.id
+        SQL
+      end
+    end
+    rows_to_array(rows)
+  end
+
+  private
 
   def init_object_from_row(row)
     if row
